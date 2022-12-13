aad520feb551a8e8a2de67d3891683b45987e263
kaylacato <119690487+kaylacato@users.noreply.github.com>
2022-12-13 18:08:12 -0500

    starter code and comments

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Write your JavaScript code here!
 
+// const { formSubmission } = require("./scriptHelper");
+
 window.addEventListener("load", function() {
 
    let listedPlanets;
@@ -12,5 +14,21 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
-   
+   let form = document.getElementByID("launchForm")
+   form.addEventListener("submit", function(event){
+    event.preventDefault();
+    // Any code in here will execute after the form submits
+    // To make this file more organized 
+    // We have to understand what has to be passed into the formSubmission function
+    // the document is the document object
+    // the pilot/copilot/fuelLevel/cargoMass we need to select these inputs using the DOM and pass in the values of these inputs into the forSubmission function
+    let pilotInput = document.getElementById("pilotName");
+    let copilotInput = document.getElementById("copilotName");
+    let fuelInput = document.getElementById("fuelLevel");
+    let cargoInput = document.getElementById("cargoMass");
+    // Alongside selecting all the inputs from the form we also want to select the faultyItems div and pass into formSubmission() 
+    let list = document.getElementById("faultyItems");
+
+    formSubmission(document, list, pilotInput.value, copilotInput.value, fuelInput.value, cargoInput.value)
+   })
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -17,11 +17,24 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+//    take in a value from one of the form inputs and returns information about whether the info is valid or not
+// if the value passed into this function is empty>> return "Empty"
+// else if the value passed is not a number>> return "Not a Number"
+// else if the value passed IS a number>> return "Is a Number"
+//  ex: isNAN(testInput) will return true if testInput is NOT a number
+//  ex: isNAN()
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+// given that we have called this function in script.js, let's assess what we now have access to in this function
+// first we passed in the global document object so now we can to document.getElemntById and other DOM functions
+// we also passed in the value of all the inputs after submitting the form
+// so we now have the ability to validate that these values are what they are supposed to be
+// what does it mean for these values to be valid?
+// Order of operations:
+//  if any of the fields are empty>>send alert to user that all fields are required
+//  else if any fields are wrong data type>>send alert to user that input is invalid
+//  else in the case we pass both of these validations then we can update the shuttle requirements  
 }
 
 async function myFetch() {
